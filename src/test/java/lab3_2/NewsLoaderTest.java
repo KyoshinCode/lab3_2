a782e3cc9d07b29db7f5660ec16db73958164365
Rafal Franiewski <194944@edu.p.lodz.pl>
2017-09-13 23:37:27 +0200

    Added dependencies to pom.xml and created empty test class

---
+++ src/test/java/lab3_2/NewsLoaderTest.java
@@ -0,0 +1,12 @@
+package lab3_2;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class NewsLoaderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
