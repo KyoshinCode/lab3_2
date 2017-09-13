7fa5d8f79bb62b0cb53b99937598abb576b637ea
Rafal Franiewski <194944@edu.p.lodz.pl>
2017-09-13 23:52:43 +0200

    Checking if public news are added

--- src/test/java/lab3_2/NewsLoaderTest.java
+++ src/test/java/lab3_2/NewsLoaderTest.java
@@ -3,20 +3,28 @@ package lab3_2;
 import edu.iis.mto.staticmock.reader.WebServiceNewsReader;
 import edu.iis.mto.staticmock.*;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.spy;
 
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class})
+@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class, PublishableNews.class})
 public class NewsLoaderTest {
 
     private WebServiceNewsReader webServiceNewsReader;
+    private PublishableNews spyPublishableNews;
 
     @Before
     public void setUp(){
@@ -31,6 +39,22 @@ public class NewsLoaderTest {
         webServiceNewsReader = Mockito.mock(WebServiceNewsReader.class);
         mockStatic(NewsReaderFactory.class);
         NewsReaderFactory newsReaderFactory = Mockito.mock(NewsReaderFactory.class);
-        Mockito.when(NewsReaderFactory.getReader(configuration.getReaderType())).thenReturn(webServiceNewsReader);
+        Mockito.when(newsReaderFactory.getReader(configuration.getReaderType())).thenReturn(webServiceNewsReader);
+
+        mockStatic(PublishableNews.class);
+        spyPublishableNews = spy(new PublishableNews());
+        Mockito.when(PublishableNews.create()).thenReturn(spyPublishableNews);
+    }
+
+    @Test
+    public void checkIfPublicNewsAreAddedTest() throws Exception{
+        IncomingNews incomingNews = new IncomingNews();
+        incomingNews.add(new IncomingInfo("Test for test", SubsciptionType.NONE));
+        Mockito.when(webServiceNewsReader.read()).thenReturn(incomingNews);
+        NewsLoader newsLoader = new NewsLoader();
+        spyPublishableNews = newsLoader.loadNews();
+        List<String> result = (List<String>) Whitebox.getInternalState(spyPublishableNews, "publicContent");
+        assertThat(result.size(), is(equalTo(1)));
+        assertThat(result.get(0), is(equalTo("Test for test")));
     }
 }
